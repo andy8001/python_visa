dc0f7dd0063715cd68efb9ed11cea848ed783783
Andreas Schmidt <schmidtan68483@th-nuernberg.de>
2020-11-21 17:58:18 +0100

    added modules and count of values function

--- modules.py
+++ modules.py
@@ -1,1 +1,16 @@
-def print_count_of_values():
+import pandas
+
+def print_count_of_values_relation(df = pandas.DataFrame):
+    index = df.index
+    number_of_rows = len(index)
+
+    headers = list(df)
+
+    for x in list(df):
+        column = (headers.pop(0))
+        print(column)
+        # display(Markdown("# " + column_object))
+        print(
+            str(df[column].count()) + "/" + str(number_of_rows) + " " + str("{:.0%}".format(df[column].count() / number_of_rows) ))
+        print('')
+
